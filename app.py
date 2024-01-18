9b95f5469ccda64c48d0fdc51a187db15dcf457c
restjun <150533379+restjun@users.noreply.github.com>
2024-01-18 16:59:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,43 @@
 from flask import Flask, render_template
-import pyupbit
-import telepot
+import requests
 import schedule
 import time
 
 app = Flask(__name__)
 
-# 텔레그램 봇 설정
-telegram_token = "6389499820:AAHCKStfe6P0FuUakX7xzEcBByaxwzD_dak"  # 텔레그램 봇 토큰
-telegram_chat_id = "@6596886700"  # 텔레그램 채널 ID
-telegram_bot = telepot.Bot(telegram_token)
+def get_top_30_tickers():
+    url = "https://api.upbit.com/v1/market/all"
+    params = {"isDetails": "false"}
+    headers = {"accept": "application/json"}
+    response = requests.get(url, params=params, headers=headers)
+    data = response.json()
 
-def send_top_30_tickers():
-    # Upbit에서 상위 30개 코인 가져오기
-    tickers = pyupbit.get_tickers("KRW")
-    top_30_tickers = tickers[:30]
+    # 거래대금이 높은 상위 30개 코인을 추출
+    top_30_tickers = sorted(data, key=lambda x: x.get("acc_trade_price_24h", 0), reverse=True)[:30]
 
-    # 메세지 내용 생성
-    message = "Top 30 Tickers:\n"
+    return top_30_tickers
+
+def print_top_30_tickers():
+    # 상위 30개 코인 정보를 얻어옴
+    top_30_tickers = get_top_30_tickers()
+
+    # 로그에 거래대금 순위 상위 30개 코인 정보 출력
+    print("거래대금 순위 상위 30개 코인:")
     for ticker in top_30_tickers:
-        message += f"{ticker}\n"
+        print(ticker["market"])
 
-    # 텔레그램으로 메세지 전송
-    telegram_bot.sendMessage(chat_id=telegram_chat_id, text=message)
+# 초기에 한번 실행
+print_top_30_tickers()
 
-# 5분마다 send_top_30_tickers 함수 실행
-schedule.every(5).minutes.do(send_top_30_tickers)
+# 15분마다 실행되도록 스케줄 설정
+schedule.every(15).minutes.do(print_top_30_tickers)
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    # Flask 앱 실행 (자동 재시작 방지)
-    app.run(debug=True, use_reloader=False, FLASK_DEBUG=True)
-
-    # 스케줄러 실행
     while True:
         schedule.run_pending()
         time.sleep(1)
+    app.run(debug=True)
